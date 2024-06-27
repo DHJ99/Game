a3c5a7600fe88d77b1134bf989bcc44e77d4919a
DHJ99 <dananjayamadushanka825@gmail.com>
2024-06-27 10:37:43 +0530

    New 4

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-function Guess(){
-    let randomNumber=Math.floor((Math.random()*10)+1);
-    console.log(randomNumber);
+let randomNumber=Math.floor((Math.random()*10)+1);
+console.log(randomNumber);
 
-    let guessNum= document.getElementById("inputNum").value;
+function Guess(){
+    let guessNum=Number(document.getElementById("inputNum").value); 
 
     if(guessNum==randomNumber){
         console.log("Your won");
-    }else if (guessNum>randomNumber){
+    }else if (guessNum>randomNumber ){
         console.log("Your Guess Num is Larger");
-    }else if (guessNum<randomNumber){
+    }else if (guessNum<randomNumber ){
         console.log("Your Guess Num is Less");
     }
 
