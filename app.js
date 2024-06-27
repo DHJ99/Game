8f1bf8e0239888b888550aa6f7ff9439511b8db1
DHJ99 <dananjayamadushanka825@gmail.com>
2024-06-27 10:11:47 +0530

    new1

---
+++ app.js
@@ -0,0 +1,13 @@
+function Guess(){
+    let randomNumber=Math.floor((Math.random()*10)+1);
+    console.log(randomNumber);
+
+    let guessNum= document.getElementById("inputNum").value;
+
+    if(guessNum==randomNumber){
+        
+    }
+
+}
+
+
